5b853e1905c81e23c5b8c56227c6e0a8abd1b150
iAmInAction <83808704+iAmInActions@users.noreply.github.com>
2021-05-13 19:43:18 +0200

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -9,7 +9,7 @@ sudo apt install nodejs npm wget nano git lua5.3
 
 # Forward ports
 sudo npm install -g localtunnel
-sudo lt --port 80 --subdomain gitcloud-pc-novnc &
+sudo lt --port 80 --subdomain actions-minecraft-client &
 
 # Start this code automatically:
 chmod +x ./gettop.sh
