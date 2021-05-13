7c10aec124c6bf02f3b8363aa2f585d20f8eeb56
iAmInAction <83808704+iAmInActions@users.noreply.github.com>
2021-05-13 19:49:41 +0200

    Update gettop.sh

--- gettop.sh
+++ gettop.sh
@@ -2,4 +2,5 @@
 cd /root
 
 # Launch script goes here:
+apt install -y xterm
 wget https://raw.githubusercontent.com/ayunami2000/play-with-docker-novnc/main/ubuntu_minecraft_launcher.sh && chmod +x ./ubuntu_minecraft_launcher.sh && ./ubuntu_minecraft_launcher.sh
