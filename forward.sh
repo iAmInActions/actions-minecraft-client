5ab74490a8a4d464c5232f817e8b46ada98fcfcd
iAmInAction <83808704+iAmInActions@users.noreply.github.com>
2021-05-19 18:29:12 +0200

    Update forward.sh

--- forward.sh
+++ forward.sh
@@ -2,6 +2,10 @@
 while true
 do
 sudo lt --port 80 --subdomain actions-minecraft-client &
+sleep 7m
+sudo lt --port 80 --subdomain actions-minecraft-client-mirror &
 sleep 15m
 sudo killall node
+sudo lt --port 80 --subdomain actions-minecraft-client-mirror &
+sleep 7m
 done
